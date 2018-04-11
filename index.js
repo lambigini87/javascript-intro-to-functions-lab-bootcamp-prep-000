3f78ce7ab55556a74a624d169b871561122fc3b5
Harrison tran <harrisontran@outlook.com>
2018-04-11 01:31:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ if (string.toLowerCase() ===  lowercase)
   return "I can\'t hear you!";
   if ((string.toUpperCase()) ===  uppercase) 
   return "YES INDEED!";
+  if (string === "I love you, Grandma.")
+  return 
 }
 
 
